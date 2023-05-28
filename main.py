4f581634ea0886870ba5f04df3f55e433ed04d2b
harunlakodla <harunlakodla@gmail.com>
2023-05-28 16:34:38 +0300

    .env file update for port

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 
 import flet as ft
+from dotenv import load_dotenv
 
 import ui.homeView.home_view as homeView
 
 
 def main(page: ft.Page):
+    load_dotenv()
     page.title = "MKT4830 | Project"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     homeView.HomeView(page).baseView()
