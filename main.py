d4b7950d217ad0cff893144c7f240262a7d19f97
harunlakodla <harunlakodla@gmail.com>
2023-05-28 16:30:56 +0300

    main file arrangment

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 import flet as ft
-import core.components.button as button
-
-import base64
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-from flet.matplotlib_chart import MatplotlibChart
 
 import ui.homeView.home_view as homeView
 
